9c3e6149fabb81edb5df20d51f719ef2ba99e642
thisorself <reservposhtaclassroom@gmail.com>
2024-12-01 05:24:42 +0300

    pdo select

--- index.php
+++ index.php
@@ -16,6 +16,13 @@
 <body>
     <?php
     include 'config.php';
+
+    $query = $pdo->prepare("SELECT * FROM real_estates WHERE id = :id");
+    $query->execute(['id' => 5]);
+    $row = $query->fetch(PDO::FETCH_OBJ);
+    print $row->id . "<br>";
+    print $row->description . "<br>";
+    print $row->price . "<br>";
     ?>
 </body>
 
